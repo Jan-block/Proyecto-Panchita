ecbf02c9f78be4e041630e73f03285949d62fba5
Karen <karen.gamion.alarcon@gmail.com>
2026-06-11 19:19:46 -0500

    corrijo conflicto AdminDashboard

--- panchita-api/src/main/java/com/example/panchita_api/controller/ReservaController.java
+++ panchita-api/src/main/java/com/example/panchita_api/controller/ReservaController.java
@@ -102,12 +102,9 @@ public class ReservaController {
         
         reservaDb.setEstadoReserva(reservaDetalles.getEstadoReserva());
         reservaDb.setObservaciones(reservaDetalles.getObservaciones());
-<<<<<<< HEAD
         reservaDb.setEstacionamiento(reservaDetalles.getEstacionamiento());
 
-=======
         
->>>>>>> 5d71d8da6bbaa920f4ab17341c5ced083400d088
         if (reservaDetalles.getMesa() != null) {
             Mesa mesaDb = mesaRepository.findById(reservaDetalles.getMesa().getId())
                     .orElseThrow(() -> new RuntimeException("Mesa no encontrada"));
